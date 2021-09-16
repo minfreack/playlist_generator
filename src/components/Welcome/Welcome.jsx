92364ae89f8c458463f71842c2d3e9e890f89e74
xminfreackx <a3518110065@alumno.uttehuacan.edu.mx>
2021-09-16 14:48:36 -0500

    Deploy

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -14,7 +14,7 @@ export const Welcome = () => {
 	const CLIENT_ID = '2ab6dff6b53145db89da10d5f4e710ee';
 	// const CLIENT_ID_SECRET = 'd8c713979e414300b4d12e0974dd1b13';
 	const AUTHORIZE = 'https://accounts.spotify.com/authorize';
-	const REDIRECT_URI = 'http://localhost:3000/generador';
+	const REDIRECT_URI = 'https://playlist-generator.vercel.app/generador';
 	const SCOPES = ['playlist-modify-public', 'playlist-modify-private', 'user-read-private', 'user-read-email'];
 	const SCOPES_URL_PARAM = SCOPES.join('%20', );
 
