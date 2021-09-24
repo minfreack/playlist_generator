a8850bd217f5d8bcbcb033e5d80127867d47d7be
xminfreackx <a3518110065@alumno.uttehuacan.edu.mx>
2021-09-23 23:11:13 -0500

    Favicon updated

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -33,6 +33,7 @@ export const App = () => {
 	return (
 		<>
 			<Helmet>
+				<title>Playlist Generator</title>
 				<link rel="preconnect" href="https://fonts.googleapis.com"/>
 				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
 				<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet"/>
