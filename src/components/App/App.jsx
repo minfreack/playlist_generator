229783f6da1b652f6279933fb3ab4085bcf63252
xminfreackx <a3518110065@alumno.uttehuacan.edu.mx>
2021-09-23 23:45:36 -0500

    Meta new

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -34,6 +34,7 @@ export const App = () => {
 		<>
 			<Helmet>
 				<title>Playlist Generator</title>
+				<meta name="description" content="Playlist Generator"/>
 				<link rel="preconnect" href="https://fonts.googleapis.com"/>
 				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
 				<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet"/>
